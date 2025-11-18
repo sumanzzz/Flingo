b5f67e4d9e01108d81160d1805e141c82bd648a5
Reegan <reegansuman@gmail.com>
2025-11-18 20:18:17 +0530

    backend setup

---
+++ flingo_b/src/main/java/com/flingo/flingo_b/FlingoBApplication.java
@@ -0,0 +1,13 @@
+package com.flingo.flingo_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlingoBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlingoBApplication.class, args);
+	}
+
+}
